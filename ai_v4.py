a427117c9ce0e0332d5c82ecb424e8aac29f5020
adam <aokruhlica@gmail.com>
2023-11-19 21:19:54 +0100

    ai_v4 Detecting avengers (80.05/144)

--- ai_v4.py
+++ ai_v4.py
@@ -15,7 +15,7 @@ class StatisticalGuesserHunt:
         self.sunk_ships = set()
 
         self.ask_fn = ask_fn
-
+        self.moves = 0
         self.shots = []
         self.queue = []
 
@@ -34,11 +34,15 @@ class StatisticalGuesserHunt:
         try:
             while len(self.queue):
                 p, y, x = heapq.heappop(self.queue)
-                if not (self.shot_board.is_one(y, x) or self.miss_board.is_one(y, x)):
+                if not (self.shot_board.is_one(y, x) and not self.miss_board.is_one(y, x)):
                     return y, x
         except IndexError:
             return None, None
         return None, None
+
+    def reset_queue(self):
+        self.queue = []
+        self.sunk_ships = set()
 
     # def enqueue_neighbors(self, y, x, priority=10):
     #     s = set()
@@ -102,17 +106,18 @@ class StatisticalGuesserHunt:
 
     def find_lines(self):
         avenger_found = self.avenger_sunk()
+
         def try_set_miss(y, x):
             if 0 <= y < self.rows:
                 if 0 <= x < self.cols:
                     if not self.hit_board.is_one(y, x):
                         self.miss_board.set_cell(y, x, 1)
 
-        def is_miss(y, x):
+        def is_miss(y, x, default=False):
             if 0 <= y < self.rows:
                 if 0 <= x < self.cols:
                     return self.miss_board.is_one(y, x)
-            return False
+            return default
 
         def is_hit(y, x):
             if 0 <= y < self.rows:
@@ -139,128 +144,234 @@ class StatisticalGuesserHunt:
                     can_expand = True
                     nexty, nextx = yy, xx
 
-                # if continues and avenger_found:
-                #     if dy == 0:
-                #         try_set_miss(yy-1,xx)
-                #         try_set_miss(yy + 1, xx)
-                    # else:
-                    #     try_set_miss(yy, xx - 1)
-                    #     try_set_miss(yy, xx + 1)
                 steps += 1
                 yy += dy
                 xx += dx
             return can_expand, (continues, steps, nexty, nextx)
 
-        lines = []
-        used = np.zeros((self.rows, self.cols))
+        def find_AC(y,x):
+            if not self.hit_board.is_one(y, x):
+                return False
+
+            expands, res1 = try_expand(y, x, horizontal=True)
+
+            # Avenger carrier horizontally (or a 5)
+            if res1[0] == 4:
+                mark_misses = [(0,-1), (0,5), (-1,-1), (-1,0), (-1,2), (-1,4), (-1,5), (1,-1), (1,0,),(1,2), (1,4),(1,5)]
+                for mark_y, mark_x in mark_misses:
+                    try_set_miss(y+mark_y, x+mark_x)
+                # try_set_miss(y, x - 1)
+                # try_set_miss(y, x + 5)
+                # try_set_miss(y - 1, x - 1)
+                # try_set_miss(y - 1, x)
+                # try_set_miss(y - 1, x + 2)
+                # try_set_miss(y - 1, x + 4)
+                # try_set_miss(y - 1, x + 5)
+                # try_set_miss(y + 1, x - 1)
+                # try_set_miss(y + 1, x)
+                # try_set_miss(y + 1, x + 2)
+                # try_set_miss(y + 1, x + 4)
+                # try_set_miss(y + 1, x + 5)
+
+                wing_positions = [(y - 1, x + 1), (y - 1, x + 3),
+                                  (y + 1, x + 1), (y + 1, x + 3)] # Possible positions of wings
+                wings_hit = 0
+                missing_wing = -1, -1
+
+                for ty, tx in wing_positions:
+                    if is_miss(ty, tx): # By process of elimination - this can't be AC
+                        self.sunk_ships.add(5) # Add the position of a sunk 5 ship
+                        for tty, ttx in wing_positions:
+                            try_set_miss(tty, ttx)
+                        return
+
+                    if is_hit(ty, tx): # By process of elimination - this is an AC
+                        wings_hit += 1
+                        for xxx in [-1, 0, 1]:
+                            for yyy in [-1, 0, 1]:
+                                try_set_miss(ty + yyy, tx + xxx)
+                    else:
+                        missing_wing = (ty, tx)
+
+                if wings_hit > 0:
+                    if missing_wing != (-1, -1):
+                        self.enqueue_one(missing_wing[0], missing_wing[1], -9)
+                        return
+                if wings_hit == 4:
+                    self.sunk_ships.add(9)
+
+            # Inspect vertical AC/5
+            expands, res2 = try_expand(y, x, horizontal=False)
+            if res2[0] == 4:  # Avenger carrier or 5
+                mark_misses = [(0, -1), (0, 5), (-1, -1), (-1, 0), (-1, 2), (-1, 4), (-1, 5), (1, -1), (1, 0,), (1, 2),
+                               (1, 4), (1, 5)]
+                for mark_x, mark_y in mark_misses:
+                    try_set_miss(y + mark_y, x + mark_x)
+                try_set_miss(y - 1, x)
+                try_set_miss(y + 5, x)
+                try_set_miss(y - 1, x - 1)
+                try_set_miss(y, x - 1)
+                try_set_miss(y, x + 1)
+                try_set_miss(y + 2, x - 1)
+                try_set_miss(y + 2, x + 1)
+                try_set_miss(y + 4, x - 1)
+                try_set_miss(y + 4, x + 1)
+                try_set_miss(y + 5, x - 1)
+                try_set_miss(y + 5, x + 1)
+
+                wing_positions = [(y + 1, x - 1), (y + 3, x - 1), (y + 1, x + 1), (y + 3, x + 1)]
+                wings_hit = 0
+                missing_wing = (-1, -1)
+
+                for ty, tx in wing_positions:
+                    if is_miss(ty, tx):
+                        self.sunk_ships.add(5)  # By process of elimination - this is an 5-ship
+                        for tty, ttx in wing_positions:
+                            try_set_miss(tty, ttx)
+                        return
+
+                    if is_hit(ty, tx):
+                        wings_hit += 1  # By process of elimination - this is an AC
+                        for xxx in [-1, 0, 1]:
+                            for yyy in [-1, 0, 1]:
+                                try_set_miss(ty + yyy, tx + xxx)
+                    else:
+                        missing_wing = (ty, tx)
+
+                if wings_hit > 0:
+                    if missing_wing != (-1, -1):
+                        self.enqueue_one(missing_wing[0], missing_wing[1], -9)
+                if wings_hit == 4:
+                    self.sunk_ships.add(9)
+
+        def find_4(y, x):
+            can_expand, res = try_expand(y, x, True, False)
+            continues, steps, nexty, nextx = res
+
+            if steps == 3:
+                both_sides_blocked = is_miss(y, x - 1, True) and is_miss(y, x + 4, True)
+                larger_ships_sunk = 9 in self.sunk_ships and 5 in self.sunk_ships
+
+                if nextx is not None and nextx <= x + 3:
+                    self.enqueue_one(y, nextx, -4)
+
+                if larger_ships_sunk or both_sides_blocked: # This is surely a ship
+                    for dy in [-1,1]:
+                        for dx in [-1, 0, 1, 2, 3, 4]:
+                            try_set_miss(y+dy, x+dx)
+                    self.sunk_ships.add(4)
+                    return
+
+            can_expand, res = try_expand(y, x, False, False)
+            continues, steps, nexty, nextx = res
+
+            if steps == 3:
+                both_sides_blocked = is_miss(y - 1, x, True) and is_miss(y+4, x, True)
+                larger_ships_sunk = 9 in self.sunk_ships and 5 in self.sunk_ships
+
+                if nexty is not None and nexty <= y + 3:
+                    self.enqueue_one(nextx, nexty, -4)
+
+                if larger_ships_sunk or both_sides_blocked:  # This is surely a ship
+                    for dy in [-1, 0, 1, 2, 3, 4]:
+                        for dx in [-1, 1]:
+                            try_set_miss(y + dy, x + dx)
+                    self.sunk_ships.add(4)
+                    return
+
+        def find_3(y, x):
+            can_expand, res = try_expand(y, x, True, False)
+            continues, steps, nexty, nextx = res
+
+            if steps == 2:
+                both_sides_blocked = is_miss(y, x - 1, True) and is_miss(y, x + 3, True)
+                larger_ships_sunk = 9 in self.sunk_ships and 5 in self.sunk_ships and 4 in self.sunk_ships #todo there's one more case to consider
+
+                if nextx is not None and nextx <= x + 2:
+                    self.enqueue_one(nexty, nextx, -3)
+
+                if larger_ships_sunk or both_sides_blocked:  # This is surely a 3 ship
+                    for dy in [-1, 1]:
+                        for dx in [-1, 0, 1, 2, 3]:
+                            try_set_miss(y + dy, x + dx)
+                    self.sunk_ships.add(3)
+                    return
+
+            can_expand, res = try_expand(y, x, False, False)
+            continues, steps, nexty, nextx = res
+
+            if steps == 2:
+                both_sides_blocked = is_miss(y - 1, x, True) and is_miss(y + 3, x, True)
+                larger_ships_sunk = 9 in self.sunk_ships and 5 in self.sunk_ships and 4 in self.sunk_ships
+
+                if nexty is not None and nexty <= y + 2:
+                    self.enqueue_one(nexty, nextx, -3)
+
+                if larger_ships_sunk or both_sides_blocked:  # This is surely a 3 ship
+                    for dx in [-1, 1]:
+                        for dy in [-1, 0, 1, 2, 3]:
+                            try_set_miss(y + dy, x + dx)
+                    self.sunk_ships.add(3)
+                    return
+
+        def find_2(y, x):
+            can_expand, res = try_expand(y, x, True, False)
+            continues, steps, nexty, nextx = res
+
+            if steps == 1:
+                both_sides_blocked = is_miss(y, x - 1, True) and is_miss(y, x + 2, True)
+
+                if nextx is not None and nextx <= x + 1:
+                    self.enqueue_one(nexty, nextx, -2)
+
+                if both_sides_blocked:  # This is surely a 3 ship
+                    for dy in [-1, 1]:
+                        for dx in [-1, 0, 1, 2]:
+                            try_set_miss(y + dy, x + dx)
+                    self.sunk_ships.add(2)
+                    return
+
+            can_expand, res = try_expand(y, x, False, False)
+            continues, steps, nexty, nextx = res
+
+            if steps == 1:
+                both_sides_blocked = is_miss(y - 1, x, True) and is_miss(y + 2, x, True)
+
+                if nexty is not None and nexty <= y + 1:
+                    self.enqueue_one(nexty, nextx, -2)
+
+                if both_sides_blocked:  # This is surely a 3 ship
+                    for dx in [-1, 1]:
+                        for dy in [-1, 0, 1, 2]:
+                            try_set_miss(y + dy, x + dx)
+                    self.sunk_ships.add(2)
+                    return
+
+
+        self.reset_queue()
+
+        # Prio 1: Detect obvious ships and potentially enqueue finishing steps
+        for fn in [find_AC]:#, find_4]: #, find_3, find_2]:
+            for y in range(self.rows):
+                for x in range(self.cols):
+                    if self.hit_board.is_one(y, x):
+                        fn(y, x)
+
+        # Prio 2: Detect unfinished lines in 4 directions and enqueue
         for y in range(self.rows):
             for x in range(self.cols):
-                if used[y, x]:
+                if not self.hit_board.is_one(y, x):
                     continue
 
-                if self.hit_board.is_one(y, x):
-                    expands, res1 = try_expand(y, x, horizontal=True)
-                    if res1[0] == 4:  # Avenger carrier or 5
-                        try_set_miss(y, x - 1)
-                        try_set_miss(y, x + 5)
-
-                        try_set_miss(y - 1, x - 1)
-                        try_set_miss(y - 1, x)
-                        try_set_miss(y - 1, x + 2)
-                        try_set_miss(y - 1, x + 4)
-                        try_set_miss(y - 1, x + 5)
-
-                        try_set_miss(y + 1, x - 1)
-                        try_set_miss(y + 1, x)
-                        try_set_miss(y + 1, x + 2)
-                        try_set_miss(y + 1, x + 4)
-                        try_set_miss(y + 1, x + 5)
-
-                        av = [(y - 1, x + 1), (y - 1, x + 3), (y + 1, x + 1), (y + 1, x + 3)]
-                        wings_hit = 0
-                        missing_wing = -1, -1
-                        for ty, tx in av:
-                            if is_miss(ty, tx):
-                                self.sunk_ships.add(5)
-                                for tty, ttx in av:
-                                    try_set_miss(tty, ttx)
-
-                            if is_hit(ty, tx):
-                                wings_hit += 1
-                                for xxx in [-1, 0, 1]:
-                                    for yyy in [-1, 0, 1]:
-                                        try_set_miss(ty + yyy, tx + xxx)
-                            else:
-                                missing_wing = (ty, tx)
-
-                        if wings_hit > 0:
-                            if missing_wing != (-1, -1):
-                                self.enqueue_one(missing_wing[0], missing_wing[1], 0)
-                                return
-                        if wings_hit == 4:
-                            self.sunk_ships.add(9)
-
-                    expands, res2 = try_expand(y, x, horizontal=False)
-                    if res2[0] == 4:  # Avenger carrier or 5
-                        try_set_miss(y - 1, x)
-                        try_set_miss(y + 5, x)
-
-                        try_set_miss(y - 1, x - 1)
-                        try_set_miss(y, x - 1)
-                        try_set_miss(y, x + 1)
-
-                        try_set_miss(y + 2, x - 1)
-                        try_set_miss(y + 2, x + 1)
-
-                        try_set_miss(y + 4, x - 1)
-                        try_set_miss(y + 4, x + 1)
-
-                        try_set_miss(y + 5, x - 1)
-                        try_set_miss(y + 5, x + 1)
-
-                        av = [(y + 1, x - 1), (y + 3, x - 1), (y + 1, x + 1), (y + 3, x + 1)]
-                        wings_hit = 0
-                        missing_wing = (-1, -1)
-                        for ty, tx in av:
-                            if is_miss(ty, tx):
-                                self.sunk_ships.add(5)
-                                for tty, ttx in av:
-                                    try_set_miss(tty, ttx)
-
-
-                            if is_hit(ty, tx):
-                                wings_hit += 1
-                                for xxx in [-1, 0, 1]:
-                                    for yyy in [-1, 0, 1]:
-                                        try_set_miss(ty + yyy, tx + xxx)
-                            else:
-                                missing_wing = (ty, tx)
-
-                        if wings_hit > 0:
-                            if missing_wing != (-1, -1):
-                                self.enqueue_one(missing_wing[0], missing_wing[1], 0)
-                                return
-                        if wings_hit == 4:
-                            self.sunk_ships.add(9)
-
-                    lines.append(res1)
-                    lines.append(res2)
-                    expands, res = try_expand(y, x, horizontal=True, reverse=True)
-                    if expands:
-                        lines.append(res)
-                    expands, res = try_expand(y, x, horizontal=False, reverse=True)
-                    if expands:
-                        lines.append(res)
-
-        lines.sort(reverse=True)
-
-        while len(lines):
-            c, l, y, x = lines.pop()
-            if c < 5 and y is not None and not self.shot_board.is_one(y, x) and not self.miss_board.is_one(y, x):
-                self.enqueue_one(y, x, priority=1)
-                return
-
+                for horiz in [False, True]:
+                    for reverse in [False, True]:
+                        expands, res = try_expand(y, x, horizontal=horiz, reverse=reverse)
+                        if expands:
+                            continues, steps, nexty, nextx = res
+                            self.enqueue_one(nexty, nextx, 0)
+        # print(self.queue)
     def move(self):
+        self.moves+=1
         self.find_lines()
         # self.analyze_board()
 
